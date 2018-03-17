b5feebd4fc1eedf3cfeef399c3b444ce12bd1963
Kyle Nash <kylenash94@gmail.com>
2018-03-17 13:26:41 +0000

    Fix autoloader and create HttpErrors model test

--- tests/Unit/Store/Eloquent/Models/HttpErrorModelTest.php
+++ tests/Unit/Store/Eloquent/Models/HttpErrorModelTest.php
@@ -1,15 +1,18 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: KyleN
- * Date: 17/03/2018
- * Time: 13:21
- */
 
 namespace Knash94\Seo\Tests\Unit\Store\Eloquent\Models;
 
 
-class HttpErrorModelTest
-{
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Knash94\Seo\Store\Eloquent\Models\HttpError;
+use Knash94\Seo\Tests\Unit\TestCase;
 
+class HttpErrorModelTest extends TestCase
+{
+    /** @test */
+    public function it_has_redirect_relationship()
+    {
+        $model = new HttpError();
+        $this->assertInstanceOf(HasOne::class, $model->redirect());
+    }
 }
