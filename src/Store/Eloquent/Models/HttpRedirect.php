735314c7fb9312db047ba0f54a4c662b71995e64
Kyle Nash <kylenash94@gmail.com>
2018-03-17 13:59:17 +0000

    Add error relationship to HttpRedirect model

--- src/Store/Eloquent/Models/HttpRedirect.php
+++ src/Store/Eloquent/Models/HttpRedirect.php
@@ -5,4 +5,14 @@ use Illuminate\Database\Eloquent\Model;
 class HttpRedirect extends Model {
 
 	protected $fillable = ['status_code', 'path', 'redirect_url'];
+
+    /**
+     * Returns the error the redirect belongs to
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function error()
+    {
+        return $this->belongsTo(HttpError::class, 'http_error_id');
+	}
 }
--- tests/Unit/Store/Eloquent/Models/HttpRedirectModelTest.php
+++ tests/Unit/Store/Eloquent/Models/HttpRedirectModelTest.php
@@ -5,6 +5,7 @@ namespace Knash94\Seo\Tests\Unit\Store\Eloquent\Models;
 
 use HttpRequest;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Knash94\Seo\Store\Eloquent\Models\HttpError;
@@ -12,7 +13,17 @@ use Knash94\Seo\Store\Eloquent\Models\HttpErrorRequest;
 use Knash94\Seo\Store\Eloquent\Models\HttpRedirect;
 use Knash94\Seo\Tests\Unit\TestCase;
 
-class HttpErrorModelTest extends TestCase
+class HttpRedirectModelTest extends TestCase
 {
+    /** @test */
+    public function it_gets_http_error()
+    {
+        $error = factory(HttpError::class)->create();
+        $model = factory(HttpRedirect::class)->create([
+            'http_error_id' => $error->id
+        ]);
 
+        $this->assertInstanceOf(BelongsTo::class, $model->error());
+        $this->assertInstanceOf(HttpError::class, $model->error);
+    }
 }
