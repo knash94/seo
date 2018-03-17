48d0efde4c17fa126b41ff40e683770f166751a7
Kyle Nash <kylenash94@gmail.com>
2018-03-17 13:55:15 +0000

    HttpError model test & factory setup

---
+++ tests/Unit/Store/Eloquent/Models/HttpRedirectModelTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Knash94\Seo\Tests\Unit\Store\Eloquent\Models;
+
+
+use HttpRequest;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Knash94\Seo\Store\Eloquent\Models\HttpError;
+use Knash94\Seo\Store\Eloquent\Models\HttpErrorRequest;
+use Knash94\Seo\Store\Eloquent\Models\HttpRedirect;
+use Knash94\Seo\Tests\Unit\TestCase;
+
+class HttpErrorModelTest extends TestCase
+{
+
+}
--- tests/Unit/TestCase.php
+++ tests/Unit/TestCase.php
@@ -5,6 +5,12 @@ namespace Knash94\Seo\Tests\Unit;
 
 class TestCase extends \Orchestra\Testbench\TestCase
 {
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->loadMigrationsFrom(__DIR__ . '\..\..\database/migrations');
+        $this->loadFactories();
+    }
     /**
      * Loads the service provider
      *
@@ -14,5 +20,10 @@ class TestCase extends \Orchestra\Testbench\TestCase
     protected function getPackageProviders($app)
     {
         return ['Knash94\Seo\SeoServiceProvider'];
+    }
+
+    protected function loadFactories()
+    {
+        $this->withFactories(__DIR__ . '\..\factories');
     }
 }
