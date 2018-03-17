34262c361ec912e1011bed73c696f285e5cf0044
Kyle Nash <kylenash94@gmail.com>
2018-03-17 13:22:52 +0000

    Remove and ignore composer.lock

---
+++ tests/Unit/Store/Eloquent/Models/HttpErrorModelTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: KyleN
+ * Date: 17/03/2018
+ * Time: 13:21
+ */
+
+namespace Knash94\Seo\Tests\Unit\Store\Eloquent\Models;
+
+
+class HttpErrorModelTest
+{
+
+}
